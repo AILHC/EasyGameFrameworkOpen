01c6fdeaa84e2e53ba09017d3855f080db0967c9
AILHC <505126057@qq.com>
2020-11-25 23:59:34 +0800

    更新模板库

--- packages/core/dist/iife/egfCore.d.ts
+++ packages/core/dist/iife/egfCore.d.ts
@@ -1,4 +1,34 @@
-declare global {
+declare namespace egfCore {
+class App<ModuleMap = any> implements egf.IApp {
+    static readonly UN_RUN: number;
+    static readonly BOOTING: number;
+    static readonly BOOTEND: number;
+    static readonly RUNING: number;
+    static readonly STOP: number;
+    protected _state: number;
+    protected _moduleMap: {
+        [key: string]: egf.IModule;
+    };
+    protected _proxyModuleMap: {
+        [key: string]: egf.IModule;
+    };
+    get state(): number;
+    get moduleMap(): ModuleMap;
+    bootstrap(bootLoaders?: egf.IBootLoader[]): Promise<boolean>;
+    init(): void;
+    loadModule(moduleIns: any | egf.IModule, key?: keyof ModuleMap): boolean;
+    hasModule(moduleKey: keyof ModuleMap): boolean;
+    stop(): void;
+    getModule<T extends egf.IModule>(moduleKey: keyof ModuleMap): T;
+    protected setState(state: number): void;
+    /**
+     * 输出
+     * @param level 1 warn 2 error
+     * @param msg
+     */
+    protected _log(msg: string, level?: number): void;
+}
+
     type ArgsType<T> = T extends (...args: infer A) => any ? A : never;
     type FunctionPropertyNames<T> = {
         [K in keyof T]: T[K] extends (...args: any[]) => any ? K : never;
@@ -73,6 +103,4 @@ declare global {
              */
             hasModule(moduleKey: keyof ModuleMap): boolean;
         }
-    }
-}
-export {};
+    }}
