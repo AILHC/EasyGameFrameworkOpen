f0519d7744b8131bea5ee73175b35609b48252f2
AILHC <505126057@qq.com>
2020-12-10 13:55:13 +0800

    1. 优化层级管理模块逻辑 2. 进行层级管理模块单元测试（通过）

--- packages/layer/__tests__/layer.test.ts
+++ packages/layer/__tests__/layer.test.ts
@@ -8,7 +8,7 @@ test("test layermgr init", function () {
     const testLayerMgr = new LayerMgr<TestRenderNode>();
     const root = new TestRenderNode();
     const addLayerSpy = jest.spyOn(testLayerMgr, "addLayer");
-    testLayerMgr.init(root, TestLayerType, TestLayerImpl);
+    testLayerMgr.init(TestLayerType, TestLayerImpl, null, root);
     expect(testLayerMgr.layerMap).toBeDefined();
     expect(addLayerSpy).toBeCalledTimes(Object.keys(TestLayerType).length / 2);
     expect(testLayerMgr.getLayerByType(TestLayerType.TestType1)).toBeDefined();
@@ -21,7 +21,7 @@ test("test layermgr init with customLayerImp", function () {
     const addLayerSpy = jest.spyOn(testLayerMgr, "addLayer");
     const classMap = new Map();
     classMap.set("TestCustomLayerImpl", TestCustomLayerImpl);
-    testLayerMgr.init(root, TestHasCustomLayerType, TestLayerImpl, classMap);
+    testLayerMgr.init(TestHasCustomLayerType, TestLayerImpl, classMap, root);
     expect(testLayerMgr.layerMap).toBeDefined();
     expect(addLayerSpy).toBeCalledTimes(Object.keys(TestHasCustomLayerType).length / 2);
     expect(testLayerMgr.getLayerByType(TestHasCustomLayerType.TestType1)).toBeDefined();
@@ -29,11 +29,30 @@ test("test layermgr init with customLayerImp", function () {
     expect(testLayerMgr.getLayerByType(TestHasCustomLayerType.TestType3)).toBeDefined();
     expect(testLayerMgr.getLayerByType(TestHasCustomLayerType.TestCustomLayerImpl_TestType4)).toBeDefined();
 })
+test("test layermgr setLayerRoot", function () {
+    const testLayerMgr = new LayerMgr<TestRenderNode>();
+    const root = new TestRenderNode();
+    const addLayerSpy = jest.spyOn(testLayerMgr, "addLayer");
+    const classMap = new Map();
+    classMap.set("TestCustomLayerImpl", TestCustomLayerImpl);
+    testLayerMgr.init(TestHasCustomLayerType, TestLayerImpl, classMap);
+
+    expect(testLayerMgr.layerMap).toBeDefined();
+    expect(addLayerSpy).toBeCalledTimes(Object.keys(TestHasCustomLayerType).length / 2);
+    expect(testLayerMgr.getLayerByType(TestHasCustomLayerType.TestType1)).toBeDefined();
+    expect(testLayerMgr.getLayerByType(TestHasCustomLayerType.TestType2)).toBeDefined();
+    expect(testLayerMgr.getLayerByType(TestHasCustomLayerType.TestType3)).toBeDefined();
+    expect(testLayerMgr.getLayerByType(TestHasCustomLayerType.TestCustomLayerImpl_TestType4)).toBeDefined();
+    expect(testLayerMgr.root).toBeUndefined();
+    testLayerMgr.setLayerRoot(root);
+    expect(testLayerMgr.root).toBeDefined();
+    expect(testLayerMgr.root.childs.length).toBe(Object.keys(TestHasCustomLayerType).length / 2);
+})
 test("test layermgr addNodeToLayer", function () {
     const testLayerMgr = new LayerMgr<TestRenderNode>();
     const root = new TestRenderNode();
     const addNodeToLayerSpy = jest.spyOn(testLayerMgr, "addNodeToLayer");
-    testLayerMgr.init(root, TestLayerType, TestLayerImpl);
+    testLayerMgr.init(TestLayerType, TestLayerImpl, null, root);
     const layer = testLayerMgr.getLayerByType(TestLayerType.TestType1);
     const onNodeAddSpy = jest.spyOn(layer, "onNodeAdd");
     testLayerMgr.addNodeToLayer(new TestRenderNode(), TestLayerType.TestType1);
@@ -44,7 +63,7 @@ test("test layermgr removeLayer", function () {
     const testLayerMgr = new LayerMgr<TestRenderNode>();
     const root = new TestRenderNode();
     const removeLayerSpy = jest.spyOn(testLayerMgr, "removeLayer");
-    testLayerMgr.init(root, TestLayerType, TestLayerImpl);
+    testLayerMgr.init(TestLayerType, TestLayerImpl, null, root);
     const layer = testLayerMgr.getLayerByType(TestLayerType.TestType1);
     const onDestroySpy = jest.spyOn(layer, "onDestroy");
     testLayerMgr.removeLayer(TestLayerType.TestType1);
@@ -56,7 +75,7 @@ test("test layermgr hideLayer", function () {
     const testLayerMgr = new LayerMgr<TestRenderNode>();
     const root = new TestRenderNode();
     const hideLayerSpy = jest.spyOn(testLayerMgr, "hideLayer");
-    testLayerMgr.init(root, TestLayerType, TestLayerImpl);
+    testLayerMgr.init(TestLayerType, TestLayerImpl, null, root);
     const layer = testLayerMgr.getLayerByType(TestLayerType.TestType1);
     const onHideSpy = jest.spyOn(layer, "onHide");
     testLayerMgr.hideLayer(TestLayerType.TestType1);
@@ -67,7 +86,7 @@ test("test layermgr showLayer", function () {
     const testLayerMgr = new LayerMgr<TestRenderNode>();
     const root = new TestRenderNode();
     const showLayerSpy = jest.spyOn(testLayerMgr, "showLayer");
-    testLayerMgr.init(root, TestLayerType, TestLayerImpl);
+    testLayerMgr.init(TestLayerType, TestLayerImpl, null, root);
     const layer = testLayerMgr.getLayerByType(TestLayerType.TestType1);
     const onShowSpy = jest.spyOn(layer, "onShow");
     testLayerMgr.showLayer(TestLayerType.TestType1);
--- packages/layer/__tests__/test-custom-layer-lmp.ts
+++ packages/layer/__tests__/test-custom-layer-lmp.ts
@@ -1,17 +1,17 @@
 import { TestRenderNode } from "./test-render-node";
 
-export class TestCustomLayerImpl implements egf.ILayer {
+export class TestCustomLayerImpl implements layer.ILayer {
     layerType: number;
     layerName: string;
     private _inited: boolean;
-    onInit(layerName: string, layerType: number, layerMgr: egf.ILayerMgr<any>): void {
+    onInit(layerName: string, layerType: number, layerMgr: layer.IMgr<any>): void {
         if (!this._inited) {
             this.layerType = layerType;
             this.layerName = layerName;
         }
     }
     onAdd(root: TestRenderNode): void {
-
+        root.addChild(this);
     }
     onHide(): void {
     }
