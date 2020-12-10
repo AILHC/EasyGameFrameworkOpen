7cdad1a6551f27fe17e85696533fbd9309c9540b
AILHC <505126057@qq.com>
2020-12-10 19:56:42 +0800

    1. 制作dpc模块自定义资源处理界面demo

--- examples/egf-ccc-full/assets/src/Utils.ts
+++ examples/egf-ccc-full/assets/src/Utils.ts
@@ -32,3 +32,35 @@ export function getChild(node: cc.Node, path: string): cc.Node {
 export function getComp<T extends cc.Component>(node: cc.Node, type: { prototype: T } | string): T {
     return node.getComponent(type as any);
 }
+/**
+ * 从数组中抽取随机元素
+ * @param arr 
+ * @param count 
+ */
+export function getRandomArrayElements(arr, count) {
+    if (arr.length == 0 || count == 0 || count > arr.length) return;
+    var shuffled = arr.slice(0), i = arr.length, min = i - count, temp, index;
+    while (i-- > min) {
+        index = Math.floor((i + 1) * Math.random());
+        temp = shuffled[index];
+        shuffled[index] = shuffled[i];
+        shuffled[i] = temp;
+    }
+    return shuffled.slice(min);
+}
+export function getSomeRandomInt(min: number, max: number, count: number): Array<number> {
+    let i, value, arr = [];
+    if ((Math.abs(max - min) + 1) < count) {
+        count = Math.abs(max - min) + 1;
+    }
+    for (i = 0; i < count; i++) {
+        value = Math.floor(Math.random() * (max - min + 1) + min);
+        if (arr.indexOf(value) < 0) {
+            arr.push(value);
+        } else {
+            i--;
+        }
+    }
+    return arr;
+    // ELog.i(arr);;
+}
---
+++ examples/egf-ccc-full/assets/tests/display-ctrl/view-ctrls/CustomResHandleView.ts
@@ -0,0 +1,74 @@
+import { NodeCtrl } from "@ailhc/dpctrl-ccc";
+import { getChild, getComp, getPrefabNodeByPath, getRandomArrayElements, getSomeRandomInt } from "../../../src/Utils";
+import { DpcTestLayerType } from "../DpcTestLayerType";
+import { dtM } from "../setDpcTestModuleMap";
+declare global {
+    interface IDpcTestViewKeyMap {
+        CustomResHandleView: string
+    }
+}
+export class CustomResHandleView extends NodeCtrl implements displayCtrl.ICustomResHandler {
+
+
+    static typeKey = "CustomResHandleView";
+    private static _ress: string[];
+    private static _monsterNames = ["BuleMonster", "GreenMonster", "PurpleMonster", "RedMonster", "YellowMonster"];
+    private static _monsterIconDir = "monster_icon";
+    private static prefabUrl: string = "display-ctrl-test-views/CustomResHandleView";
+    private _monsterIconRess: string[];
+    loadRes(onComplete: VoidFunction, onError: VoidFunction): void {
+        dtM.uiMgr.showDpc({
+            typeKey: dtM.uiMgr.ctrlKeys.LoadingView,
+            showedCb: () => {
+                const randomMonsterNameIndexs = getSomeRandomInt(0, CustomResHandleView._monsterNames.length, 2);
+                const ress = [];
+                this._monsterIconRess = ress;
+                randomMonsterNameIndexs.forEach(element => {
+                    ress.push(CustomResHandleView._monsterIconDir + "/" + CustomResHandleView._monsterNames[element]);
+                });
+                ress.push(CustomResHandleView.prefabUrl);
+                cc.assetManager.loadAny(ress,
+                    (finished: number, total: number, item) => {
+                        dtM.uiMgr.updateDpc(dtM.uiMgr.ctrlKeys.LoadingView,
+                            {
+                                finished: finished, total: total
+                            })
+                    }, (err, data) => {
+                        if (err) {
+                            onError();
+                        } else {
+                            onComplete()
+                        }
+                    })
+            }
+        })
+    }
+    releaseRes(): void {
+        cc.assetManager.releaseAsset(cc.resources.get(CustomResHandleView.prefabUrl));
+    }
+    onInit() {
+        super.onInit()
+        this.node = getPrefabNodeByPath(CustomResHandleView.prefabUrl);
+        
+    }
+    onShow(data?: any) {
+        super.onShow();
+        dtM.layerMgr.addNodeToLayer(this.node, DpcTestLayerType.POP_UP_UI);
+        const monsterNodeA = getChild(this.node, "monsterA");
+        const monsterNodeB = getChild(this.node, "monsterB");
+        const monsterSpCompA = getComp(monsterNodeA, cc.Sprite);
+        monsterSpCompA.spriteFrame = cc.resources.get(this._monsterIconRess[0], cc.SpriteFrame);
+        const monsterSpCompB = getComp(monsterNodeB, cc.Sprite);
+        monsterSpCompB.spriteFrame = cc.resources.get(this._monsterIconRess[1], cc.SpriteFrame);
+    }
+    onHide() {
+        super.onHide();
+    }
+    onDestroy(destroyRes?: boolean) {
+        super.onDestroy();
+        if (destroyRes) {
+            cc.assetManager.releaseAsset(cc.resources.get<cc.Prefab>(CustomResHandleView.prefabUrl, cc.Prefab));
+        }
+    }
+
+}
