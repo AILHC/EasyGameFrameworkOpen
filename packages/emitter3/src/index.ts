3ac4246c472b2d015719d9dbd1e3b9cb28579d53
AILHC <505126057@qq.com>
2021-01-02 14:46:59 +0800

    增加一个给c3d用的eventemitter3库

---
+++ packages/emitter3/src/index.ts
@@ -0,0 +1,19 @@
+import * as Emitter3 from "eventemitter3"
+import {
+    EventArgs, EventListener, EventNames,
+    ValidEventTypes, ArgumentMap, ListenerFn,
+    EventEmitterStatic,
+    EventEmitter,
+
+} from "eventemitter3";
+const EventEmitter3 = Emitter3.default;
+export {
+    EventEmitter3,
+    EventArgs,
+    EventListener,
+    EventNames,
+    ValidEventTypes,
+    ArgumentMap,
+    ListenerFn,
+    EventEmitterStatic,
+};
