ba04ddf712bc972f529305ecd93433afe84a603b
AILHC <505126057@qq.com>
2021-01-25 19:15:46 +0800

    egf-net-ws 增加放烟花效果

--- examples/egf-net-ws/egf-simple-net-client/index.js
+++ examples/egf-net-ws/egf-simple-net-client/index.js
@@ -1,19 +1,40 @@
-import {
-    NetNode
-} from "https://cdn.jsdelivr.net/npm/@ailhc/enet@1.0.0/dist/es/lib/index.js"
+// import {
+//     NetNode
+// } from "https://cdn.jsdelivr.net/npm/@ailhc/enet@1.0.0/dist/es/lib/index.js"
 
 
 /**
 * @type import("@ailhc/enet").NetNode<string>
 */
-var enet = new NetNode()
-enet.init({
-    netEventHandler: {}
+var netNode = new enet.NetNode()
+netNode.init({
+    netEventHandler: {
+        onStartConnenct: () => {
+            console.log(`开始连接服务器`);
+        },
+        onConnectEnd: () => {
+            console.log(`连接服务器成功👌`);
+        }
+    }
 });
-enet.connect("wss://echo.websocket.org/");
+netNode.connect("wss://echo.websocket.org/");
 
-window.enet = enet;
-window.sendMsgToServer = function (msg) { enet.notify("msg", msg); }
-enet.onPush("msg", function (dpkg) {
+window.netNode = netNode;
+window.sendMsgToServer = function (msg) {
+    if (!netNode.socket.isConnected) {
+        console.warn(`服务器还没连上`);
+        return;
+    }
+    netNode.notify("msg", msg);
+    checkAndFire(msg);
+}
+netNode.onPush("msg", function (dpkg) {
     console.log(`服务器返回:`, dpkg.data);
+    checkAndFire(dpkg.data);
 })
+
+function checkAndFire(msg) {
+    if (msg.includes("烟花") | msg.includes("🎇")) {
+        fire(window.innerWidth * 2 / 3, window.innerHeight / 2);
+    }
+}
