8e8b9f4926c82904256e078d10b7b88e5f17c4cc
AILHC <505126057@qq.com>
2021-02-15 13:18:25 +0800

    fix: 修复后续加载模块初始化失败bug
    affects: egf-ccc-full

--- examples/egf-ccc-full/assets/src/AppMainComp.ts
+++ examples/egf-ccc-full/assets/src/AppMainComp.ts
@@ -1,24 +1,20 @@
-
 const { ccclass, property } = cc._decorator;
 import { App } from "@ailhc/egf-core";
 import { FrameworkLoader } from "./FrameworkLoader";
 import { m, setModuleMap } from "./ModuleMap";
 @ccclass
 export default class AppMainComp extends cc.Component {
-
     onLoad() {
         this.bootFramework();
-        
     }
-    bootFramework() {
+    async bootFramework() {
         const app = new App();
-        app.bootstrap([new FrameworkLoader()]);
-        
+        await app.bootstrap([new FrameworkLoader()]);
+
         setModuleMap(app.moduleMap);
 
         app.init();
 
-        window["m"] = m;//方便调试用
-
+        window["m"] = m; //方便调试用
     }
 }
