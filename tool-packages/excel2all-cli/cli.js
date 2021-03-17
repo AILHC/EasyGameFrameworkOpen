f666a1da8d66e4b185de9f90ff12f24ec28cc58d
AILHC <505126057@qq.com>
2021-03-17 18:03:42 +0800

    fix: 完善cli参数解析，调整代码结构
    affects: @ailhc/excel2all-cli

--- tool-packages/excel2all-cli/cli.js
+++ tool-packages/excel2all-cli/cli.js
@@ -4,6 +4,7 @@ const path = require("path");
 const excel2all = require("@ailhc/excel2all");
 
 const package = require("./package.json");
+const getParseConfig = require("./get-parse-config")
 program
     .version(package.version)
     .description(`一个将excel文件转换为typescript声明文件和json工具
@@ -17,7 +18,7 @@ program
     .option('-c, --config [config]', '导表配置文件路径，如果没有则使用默认的\n-------------------------')
     .option('-t, --tableFileDir [tableFileDir]', "excel文件夹路径,如果没有则使用项目根目录\n-------------------------")
     .option('-pt, --pattern [pattern...]', 'excel文件名匹配规则,多个以空格隔开 以 -- 结束,\n如: -pt "\\**\\*.{xlsx,csv}" "!~$*.*" -- \n默认匹配规则["\\**\\*.{xlsx,csv}", "!~$*.*"]\n匹配所有后缀为.xlsx和.csv的文件，如果符合~$*.* 或~.*.* 则排除\n参考：https://github.com/micromatch/micromatch\n--------')
-    .option('-c, --useCache', '是否使用缓存，默认不\n-------------------------')
+    .option('-uc, --useCache', '是否使用缓存，默认不\n-------------------------')
     .option('-cf, --cacheFileDirPath [cacheFileDirPath]', '缓存文件文件夹，\n-------------------------')
     .option('-mt, --useMultiThread', '是否使用多线程，默认不')
     .option('-tn, --threadParseFileMaxNum [threadParseFileMaxNum]', '单个线程解析文件的最大数量\n-------------------------')
@@ -39,71 +40,9 @@ program
     .option('-bdn, --bundleDtsFileName [bundleDtsFileName]', '合并后的声明文件名\n-------------------------')
     .option('-cj, --isCompress', '是否将json格式压缩,默认否,减少json字段名字符,效果较小\n-------------------------')
 
-    .action(
-        /**
-         * 
-         * @param {ITableParseConfig & IOutputConfig &{ config:string }} option 
-         */
-        function (option) {
-            /**
-             * @type {ITableParseConfig}
-             */
-            let config = {};
-            if (!option.projRoot) {
-                option.projRoot = process.cwd();
-            } else if (!path.isAbsolute(option.projRoot)) {
-                option.projRoot = path.join(process.cwd(), option.projRoot);
-            }
-            if (option.config) {
-                if (!path.isAbsolute(option.config)) {
-                    option.config = path.join(option.projRoot, option.config);
-                }
-                config = require(option.config);
-            } else {
-                config = option;
-            }
-            if (!config) {
-                console.error(`配置路径不存在:${option.config}`);
-                return;
-            }
-            if (!config.projRoot) {
-                config.projRoot = option.projRoot;
-            }
-            if (!config.tableFileDir) {
-                config.tableFileDir = config.projRoot;
-            } else if (!path.isAbsolute(config.tableFileDir)) {
-                config.tableFileDir = path.join(config.projRoot, config.tableFileDir);
-            }
-
-            if (!config.outputConfig) {
-                /**
-                 * @type {IOutputConfig}
-                 */
-                config.outputConfig = {
-                    clientSingleTableJsonDir: option.clientSingleTableJsonDir,
-                    clientBundleJsonOutPath: option.clientBundleJsonOutPath,
-                    isFormatBundleJson: option.isFormatBundleJson,
-                    isGenDts: option.isGenDts,
-                    clientDtsOutDir: option.clientDtsOutDir,
-                    isBundleDts: option.isBundleDts,
-
-                    bundleDtsFileName: option.bundleDtsFileName,
-                    isCompress: option.isCompress
-                }
-                for(let key in config.outputConfig){
-                    delete config[key];
-                }
-            }
-
-            /**
-             * @type {IOutputConfig}
-             */
-            const outputConfig = config.outputConfig;
-            if (!outputConfig.clientSingleTableJsonDir) {
-                outputConfig.clientSingleTableJsonDir = path.join(process.cwd(), "./excelJsonOut");
-            }
-            if (!outputConfig.bundleDtsFileName) {
-                outputConfig.bundleDtsFileName = "tableMap";
-            }
+    .action(function (option) {
+        const config = getParseConfig(option);
+        if (config) {
             excel2all.convert(config)
-        }).parse();
+        }
+    }).parse();
