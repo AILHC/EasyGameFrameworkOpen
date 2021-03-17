39fdc3e6252eb8ab6378951c3a854cb90e3132c7
AILHC <505126057@qq.com>
2021-03-17 18:03:42 +0800

    fix: 完善cli参数解析，调整代码结构
    affects: @ailhc/excel2all-cli

---
+++ tool-packages/excel2all-cli/get-parse-config.js
@@ -0,0 +1,87 @@
+const path = require("path");
+/**
+ * @param {ITableConvertConfig & IOutputConfig &{ config:string }} option 
+ */
+function getParseConfig(option) {
+    /**
+     * @type {ITableConvertConfig}
+     */
+    let config = {};
+    option.projRoot = getAbsolutePath(option.projRoot, process.cwd());
+    if (typeof option.config === "string") {
+        if (!path.isAbsolute(option.config)) {
+            option.config = path.join(option.projRoot, option.config);
+        }
+        config = require(option.config);
+    } else {
+        config = option;
+    }
+    if (!config) {
+        console.error(`配置路径不存在:${option.config}`);
+        return;
+    }
+    if (!config.projRoot) {
+        config.projRoot = option.projRoot;
+    }
+    if (!config.tableFileDir) {
+        config.tableFileDir = config.projRoot;
+    } else if (!path.isAbsolute(config.tableFileDir)) {
+        config.tableFileDir = path.join(config.projRoot, config.tableFileDir);
+    }
+
+    if (!config.outputConfig) {
+        /**
+         * @type {IOutputConfig}
+         */
+        const outputConfig = {
+            clientSingleTableJsonDir: option.clientSingleTableJsonDir,
+            clientBundleJsonOutPath: option.clientBundleJsonOutPath,
+            isFormatBundleJson: option.isFormatBundleJson,
+            isGenDts: option.isGenDts,
+            clientDtsOutDir: option.clientDtsOutDir,
+            isBundleDts: option.isBundleDts,
+
+            bundleDtsFileName: option.bundleDtsFileName,
+            isCompress: option.isCompress
+        }
+
+        for (let key in outputConfig) {
+            delete config[key];
+        }
+        outputConfig.clientSingleTableJsonDir = getAbsolutePath(outputConfig.clientSingleTableJsonDir, config.projRoot, "./excelJsonOut");
+        if (outputConfig.isGenDts) {
+            outputConfig.clientDtsOutDir = getAbsolutePath(outputConfig.clientDtsOutDir, config.projRoot);
+            if (!outputConfig.bundleDtsFileName) {
+                outputConfig.bundleDtsFileName = "tableMap";
+            }
+        }
+        if (outputConfig.clientBundleJsonOutPath) {
+            outputConfig.clientBundleJsonOutPath = getAbsolutePath(outputConfig.clientBundleJsonOutPath, config.projRoot);
+        }
+
+        config.outputConfig = outputConfig;
+    }
+
+    return config;
+}
+/**
+ * 获取绝对路径
+ * @param {*} originPath 
+ * @param {*} root 
+ * @param {*} defaultPath 
+ */
+function getAbsolutePath(originPath, root, defaultPath) {
+    if (!originPath) {
+        if (defaultPath) {
+            return path.join(root, defaultPath);
+        } else {
+            return root;
+        }
+
+    } else if (!path.isAbsolute(originPath)) {
+        return path.join(root, originPath);
+    } else {
+        return originPath;
+    }
+}
+module.exports = getParseConfig;
