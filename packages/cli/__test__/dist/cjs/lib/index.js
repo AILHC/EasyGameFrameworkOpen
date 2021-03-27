03412a7689dfde1eae7f4dd18c87d8dc09b2e65c
AILHC <505126057@qq.com>
2021-03-27 15:14:56 +0800

    feat: 1. 增加自定义配置功能 2. 完善原本的编译处理
    affects: @ailhc/egf-cli
    
    1. 对之前的编译没太大影响

--- packages/cli/__test__/dist/cjs/lib/index.js
+++ packages/cli/__test__/dist/cjs/lib/index.js
@@ -2,38 +2,23 @@
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
-var classA = /** @class */ (function () {
-    function classA() {
-    }
-    /**
-     * ffjfjf
-     * @param a
-     */
-    classA.prototype.sayA = function (a) { };
-    return classA;
-}());
+class classA {
+    sayA(a) { }
+}
 
-var classB = /** @class */ (function () {
-    function classB() {
-    }
-    classB.prototype.sayb = function (b) {
+class classB {
+    sayb(b) {
         console.log("FDS");
-    };
-    return classB;
-}());
-
-// import {Generator} from  "npm-dts"
-var classC = /** @class */ (function () {
-    function classC() {
     }
-    classC.prototype.sayd = function (cc) {
+}
+
+class classC {
+    sayd(cc) {
         console.log(cc);
-        // new Generator();
-    };
-    return classC;
-}());
+    }
+}
 
 exports.classA = classA;
 exports.classB = classB;
 exports.classC = classC;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VzIjpbXSwic291cmNlc0NvbnRlbnQiOltdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7In0=
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VzIjpbXSwic291cmNlc0NvbnRlbnQiOltdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7In0=
--- packages/cli/__test__/dist/cjs/types/index.d.ts
+++ packages/cli/__test__/dist/cjs/types/index.d.ts
@@ -1,48 +1,56 @@
-declare module "@ailhc/egf-cli/src/classA" {
-    export class classA {
-        /**
-         * ffjfjf
-         * @param a
-         */
-        sayA(a: number): void;
-    }
+declare module '@ailhc/egf-cli/src/classA' {
+	export class classA {
+	    /**
+	     * ffjfjf
+	     * @param a
+	     */
+	    sayA(a: number): void;
+	}
+
 }
-declare module "@ailhc/egf-cli/src/classB" {
-    export class classB {
-        sayb(b: string): void;
-    }
+declare module '@ailhc/egf-cli/src/classB' {
+	export class classB {
+	    sayb(b: string): void;
+	}
+
 }
-declare module "@ailhc/egf-cli/src/classC" {
-    export class classC {
-        sayd(cc: any): void;
-    }
+declare module '@ailhc/egf-cli/src/classC' {
+	export class classC {
+	    sayd(cc: any): void;
+	}
+
 }
-declare module "@ailhc/egf-cli" {
-    export * from "@ailhc/egf-cli/src/classA";
-    export * from "@ailhc/egf-cli/src/classB";
-    export * from "@ailhc/egf-cli/src/classC";
+declare module '@ailhc/egf-cli' {
+	export * from '@ailhc/egf-cli/src/classA';
+	export * from '@ailhc/egf-cli/src/classB';
+	export * from '@ailhc/egf-cli/src/classC';
+
 }
-declare module "@ailhc/egf-cli/src2/classA" {
-    export class classA {
-        /**
-         * ffjfjf
-         * @param a
-         */
-        sayA(a: number): void;
-    }
+declare module '@ailhc/egf-cli/src2/classA' {
+	export class classA {
+	    /**
+	     * ffjfjf
+	     * @param a
+	     */
+	    sayA(a: number): void;
+	}
+
 }
-declare module "@ailhc/egf-cli/src2/classB" {
-    export class classB {
-        sayb(b: string): void;
-    }
+declare module '@ailhc/egf-cli/src2/classB' {
+	export class classB {
+	    sayb(b: string): void;
+	}
+
 }
-declare module "@ailhc/egf-cli/src2/classD" {
-    export class classD {
-        sayb(b: string): void;
-    }
+declare module '@ailhc/egf-cli/src2/classD' {
+	export class classD {
+	    sayb(b: string): void;
+	}
+
 }
-declare module "@ailhc/egf-cli/src2/index" {
-    export * from "@ailhc/egf-cli/src/classA";
-    export * from "@ailhc/egf-cli/src/classB";
-    export * from "@ailhc/egf-cli/src/classD";
+declare module '@ailhc/egf-cli/src2/index' {
+	export * from '@ailhc/egf-cli/src/classA';
+	export * from '@ailhc/egf-cli/src/classB';
+	export * from '@ailhc/egf-cli/src/classD';
+
 }
