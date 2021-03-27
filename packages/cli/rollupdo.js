c9790713e24d82e27ed5e30723612a544e83e96e
AILHC <505126057@qq.com>
2021-03-27 21:09:51 +0800

    feat: 支持自定义插件
    affects: @ailhc/egf-cli

--- packages/cli/rollupdo.js
+++ packages/cli/rollupdo.js
@@ -9,8 +9,9 @@ const fs = require("fs");
 const dtsGenerator = require('dts-generator');
 const jsonPlugin = require("@rollup/plugin-json");
 const terser = require("rollup-plugin-terser");
-var curPackFiles = null;  //当前包的所有的文件
+var curPackFiles = null; //当前包的所有的文件
 var mentry = 'multientry:entry-point';
+
 function myMultiInput() {
     var include = [];
     var exclude = [];
@@ -42,71 +43,69 @@ function myMultiInput() {
         return `export * from ${JSON.stringify(p)};`;
     };
 
-    return (
-        {   //解析调用rollup时传入的配置
-            options(options) {
-                configure(options.input);
-                options.input = mentry;
-            },
-            //解决每个文件里的引用
-            //id 被引用的文件，importer引用者
-            resolveId(id, importer) {//entry是个特殊字符串，rollup并不识别，所以假装这里解析一下
-                if (id === mentry) {//被引用的是多路径就返回
-                    return mentry;
+    return ({ //解析调用rollup时传入的配置
+        options(options) {
+            configure(options.input);
+            options.input = mentry;
+        },
+        //解决每个文件里的引用
+        //id 被引用的文件，importer引用者
+        resolveId(id, importer) { //entry是个特殊字符串，rollup并不识别，所以假装这里解析一下
+            if (id === mentry) { //被引用的是多路径就返回
+                return mentry;
+            }
+            if (mentry == importer) //引用者是多路径集合返回
+                return;
+            //以下处理，是Laya针对，引用的文件不存在于这个包内的，返回Laya的标识，当作全局引用处理
+            var importfile = path.join(path.dirname(importer), id);
+            var ext = path.extname(importfile);
+            if (ext != '.ts' && ext != '.glsl' && ext != '.vs' && ext != '.ps' && ext != '.fs') {
+                importfile += '.ts';
+            }
+            //console.log('import ', importfile);
+            // if (curPackFiles.indexOf(importfile) < 0) {
+            //     //其他包里的文件
+            //     // console.log('other pack:',id,'importer=', importer);
+            //     return 'Laya';
+            // }
+        },
+        load(id) {
+            //id加载的文件名
+            if (id === mentry) {
+                //是多路径集合
+                if (!include.length) {
+                    return Promise.resolve('');
                 }
-                if (mentry == importer)//引用者是多路径集合返回
-                    return;
-                //以下处理，是Laya针对，引用的文件不存在于这个包内的，返回Laya的标识，当作全局引用处理
-                var importfile = path.join(path.dirname(importer), id);
-                var ext = path.extname(importfile);
-                if (ext != '.ts' && ext != '.glsl' && ext != '.vs' && ext != '.ps' && ext != '.fs') {
-                    importfile += '.ts';
-                }
-                //console.log('import ', importfile);
-                // if (curPackFiles.indexOf(importfile) < 0) {
-                //     //其他包里的文件
-                //     // console.log('other pack:',id,'importer=', importer);
-                //     return 'Laya';
-                // }
-            },
-            load(id) {
-                //id加载的文件名
-                if (id === mentry) {
-                    //是多路径集合
-                    if (!include.length) {
-                        return Promise.resolve('');
-                    }
-                    //拼接匹配正则
-                    var patterns = include.concat(exclude.map(function (pattern) {
-                        return '!' + pattern;
-                    }));
-                    //匹配出符合规则的路径集合
-                    return matched.promise(patterns, { realpath: true }).then(function (paths) {
-                        curPackFiles = paths;   // 记录一下所有的文件
-                        paths.sort();//将路径集合排序一下
-                        /**
-                         * 遍历生成
-                         * 类似
-                         * export * from "<path1>";
-                         * export * from "<path2>";
-                         */
-                        const matchExportResult = paths.map(exporter).join('\n');
-                        return matchExportResult
-                    });
-                } else {
-                    //console.log('load ',id);
-                }
+                //拼接匹配正则
+                var patterns = include.concat(exclude.map(function(pattern) {
+                    return '!' + pattern;
+                }));
+                //匹配出符合规则的路径集合
+                return matched.promise(patterns, { realpath: true }).then(function(paths) {
+                    curPackFiles = paths; // 记录一下所有的文件
+                    paths.sort(); //将路径集合排序一下
+                    /**
+                     * 遍历生成
+                     * 类似
+                     * export * from "<path1>";
+                     * export * from "<path2>";
+                     */
+                    const matchExportResult = paths.map(exporter).join('\n');
+                    return matchExportResult
+                });
+            } else {
+                //console.log('load ',id);
             }
         }
-    );
+    });
 }
 const tsconfigOverride = {
-    compilerOptions: {}
-}
-/**
- * 构建编译
- * @param {IEgfCompileOption} option 
- */
+        compilerOptions: {}
+    }
+    /**
+     * 构建编译
+     * @param {IEgfCompileOption} option 
+     */
 async function rollupBuild(option) {
     // projRoot, isWatch, entrys, outputDir, output,
     // format, typesDir, unRemoveComments, target, minify, isGenDts, banner
@@ -135,7 +134,9 @@ async function rollupBuild(option) {
         try {
             optionOverride = require(configPath)
         } catch (error) {
-            console.warn(`[warning]没有配置文件:${configPath}`)
+            console.warn(`[warning]没有配置文件或配置文件有问题:${configPath}`);
+            console.warn(error)
+
         }
     }
 
@@ -239,68 +240,68 @@ async function rollupBuild(option) {
      * @type {import('rollup-plugin-typescript2/dist/ioptions').IOptions}
      */
     const tsOptions = {
-        clean: true,
-        abortOnError: false,
-        tsconfigOverride: tsconfigOverride,
-        useTsconfigDeclarationDir: true,
-    }
-
-
+            clean: true,
+            abortOnError: false,
+            tsconfigOverride: tsconfigOverride,
+            useTsconfigDeclarationDir: true,
+        }
+        //自定义插件
+    const customPlugins = option.plugins ? option.plugins : [];
     /**
      * @type {import('rollup').InputOptions}
      */
     const buildConfig = {
-        //输出log便于调试
-        onwarn: function ({ loc, frame, message }) {
-            // 打印位置（如果适用）
-            if (loc) {
-                console.warn(`${loc.file} (${loc.line}:${loc.column}) ${message}`);
-                if (frame) console.warn(frame);
-            } else {
-                console.warn(message);
-            }
-        },
-        external: (source,
-            importer,
-            isResolved) => {
-            if (!externalTag || externalTag === "") return false;
-            let tag = externalTag;
-            let isExternal = false;
-            if (typeof externalTag === "object" && externalTag.length) {
-                for (let i = 0; i < externalTag.length; i++) {
-                    tag = externalTag[i];
+            //输出log便于调试
+            onwarn: function({ loc, frame, message }) {
+                // 打印位置（如果适用）
+                if (loc) {
+                    console.warn(`${loc.file} (${loc.line}:${loc.column}) ${message}`);
+                    if (frame) console.warn(frame);
+                } else {
+                    console.warn(message);
+                }
+            },
+            external: (source,
+                importer,
+                isResolved) => {
+                if (!externalTag || externalTag === "") return false;
+                let tag = externalTag;
+                let isExternal = false;
+                if (typeof externalTag === "object" && externalTag.length) {
+                    for (let i = 0; i < externalTag.length; i++) {
+                        tag = externalTag[i];
+                        isExternal = source.includes(tag);
+                        if (isExternal) break;
+                    }
+                } else {
                     isExternal = source.includes(tag);
-                    if (isExternal) break;
                 }
-            } else {
-                isExternal = source.includes(tag);
-            }
-            if (isExternal) {
-                console.log(`'${source}' is imported by ${importer}, is custom external  – treating it as an external dependency`)
+                if (isExternal) {
+                    console.log(`'${source}' is imported by ${importer}, is custom external  – treating it as an external dependency`)
 
-            }
-            return isExternal;
-
-        },
-        input: entrys,
-        plugins: [
-            // myMultiInput(),
-            jsonPlugin(),
-            typescript(tsOptions),
-            rollupCjs(),
-            nodeResolve({
-                customResolveOptions: {
-                    moduleDirectory: "node_modules"
                 }
-            })
-            // rdts()
-        ]
-    }
-    // /**
-    //  * @type { import('rollup').OutputOptions[] }
-    //  */
-    // const outputOpts = [];
-    // for (let i = 0; i < outputs.length; i++) {
+                return isExternal;
+
+            },
+            input: entrys,
+            plugins: [
+                // myMultiInput(),
+                jsonPlugin(),
+                typescript(tsOptions),
+                rollupCjs(),
+                nodeResolve({
+                    customResolveOptions: {
+                        moduleDirectory: "node_modules"
+                    }
+                })
+                // rdts()
+            ].concat(customPlugins)
+        }
+        // /**
+        //  * @type { import('rollup').OutputOptions[] }
+        //  */
+        // const outputOpts = [];
+        // for (let i = 0; i < outputs.length; i++) {
 
     //     outputOpts.push(outputOption);
     // }
@@ -308,15 +309,15 @@ async function rollupBuild(option) {
     const footerStr = option.footer ? option.footer :
         (moduleName && useFooter ?
             `var globalTarget =window?window:global;
-             globalTarget.${moduleName}?Object.assign({},globalTarget.${moduleName}):(globalTarget.${moduleName} = ${moduleName})`
-            : '');
+             globalTarget.${moduleName}?Object.assign({},globalTarget.${moduleName}):(globalTarget.${moduleName} = ${moduleName})` :
+            '');
 
     /**
      * @type { import('rollup').OutputOptions }
      */
     let outputOption = {
         file: output,
-        chunkFileNames: "[name].js",//公共模块生成规则
+        chunkFileNames: "[name].js", //公共模块生成规则
         entryFileNames: (chunkInfo) => {
             if (format === "es" || format === "esm") {
                 return `[name].mjs`
@@ -357,8 +358,7 @@ async function rollupBuild(option) {
             } else if (event.code === "BUNDLE_END") {
                 console.log("构建结束");
                 isGenDts && customDts && genCustomDts(projRoot, format, typesDir, moduleName);
-            }
-            else if (event.code === "END") {
+            } else if (event.code === "END") {
                 console.log(`结束`)
             } else if (event.code === "ERROR") {
                 console.error(`构建错误`, event.error);
@@ -381,8 +381,8 @@ async function rollupBuild(option) {
         const minify = option.minify;
         if (minify) {
             /**
-                * @type {import("rollup-plugin-terser").Options}
-                */
+             * @type {import("rollup-plugin-terser").Options}
+             */
             const terserOption = {
                 output: {
                     ascii_only: true // 仅输出ascii字符,
@@ -433,7 +433,7 @@ function genDts(projRoot, entrys, format, typesDir, moduleName) {
             exclude: dtsGenExclude,
             out: path.join(typesDirPath, `index.d.ts`),
             // prefix: moduleName,
-            resolveModuleId: function (params) {
+            resolveModuleId: function(params) {
                 // console.log(params.currentModuleId)
                 let entryRelative = path.relative(projRoot, entry);
                 if (path.sep === "\\") {
@@ -444,7 +444,7 @@ function genDts(projRoot, entrys, format, typesDir, moduleName) {
                 }
                 return `${moduleName}/${params.currentModuleId}`
             },
-            resolveModuleImport: function (params) {
+            resolveModuleImport: function(params) {
                 // console.log(params)
                 // {
                 //     importedModuleId: './interfaces',
@@ -476,7 +476,7 @@ function genDts(projRoot, entrys, format, typesDir, moduleName) {
  * 生成单个全局dts
  * @param {string} projRoot 
  * @param {string} format
-* @param {string} typesDir 
+ * @param {string} typesDir 
  * @param {string} moduleName 
  */
 function genCustomDts(projRoot, format, typesDir, moduleName) {
@@ -490,44 +490,44 @@ function genCustomDts(projRoot, format, typesDir, moduleName) {
     const typesDirPath = path.join(projRoot, typesDir);
     const declareGlobalRegex = /(declare global \{{1})([\s\S]*\}{1})([\s]*\}{1})/g;
     const importRegex = /import\s?{\s?[\w\d]*?\s?}\s?from\s?"[\w\W]*?";?/g
-    fs.readdir(typesDirPath, function (err, files) {
-        let str;
-        let regexMatch;
-        for (let i = 0; i < files.length; i++) {
-            if (files[i] === "index.d.ts") continue;
-            // if (files[i] === "index.d.ts" || files[i].includes("interface")) continue;
-            const typeFilePath = path.join(typesDirPath, files[i]);
-            // console.log(typeFilePath);
-            str = fs.readFileSync(path.join(typesDirPath, files[i]), "utf8");
-            regexMatch = importRegex.exec(str);
-            if (regexMatch) {
-                regexMatch.forEach(function (match) {
-                    str = str.replace(match, "")
-                })
+    fs.readdir(typesDirPath, function(err, files) {
+            let str;
+            let regexMatch;
+            for (let i = 0; i < files.length; i++) {
+                if (files[i] === "index.d.ts") continue;
+                // if (files[i] === "index.d.ts" || files[i].includes("interface")) continue;
+                const typeFilePath = path.join(typesDirPath, files[i]);
+                // console.log(typeFilePath);
+                str = fs.readFileSync(path.join(typesDirPath, files[i]), "utf8");
+                regexMatch = importRegex.exec(str);
+                if (regexMatch) {
+                    regexMatch.forEach(function(match) {
+                        str = str.replace(match, "")
+                    })
+                }
+                regexMatch = declareGlobalRegex.exec(str)
+                if (regexMatch) {
+                    dtsStr += regexMatch[2];
+                } else {
+                    dtsStr += str;
+                }
+
             }
-            regexMatch = declareGlobalRegex.exec(str)
-            if (regexMatch) {
-                dtsStr += regexMatch[2];
-            } else {
-                dtsStr += str;
-            }
+            dtsStr = dtsStr.replace(/export {}/g, "");
+            dtsStr = dtsStr.replace(/export declare /g, "");
+            dtsStr = dtsStr.replace(/export default /g, "");
 
-        }
-        dtsStr = dtsStr.replace(/export {}/g, "");
-        dtsStr = dtsStr.replace(/export declare /g, "");
-        dtsStr = dtsStr.replace(/export default /g, "");
+            dtsStr = `declare namespace ${moduleName} {\n` + dtsStr + "}\n";
 
-        dtsStr = `declare namespace ${moduleName} {\n` + dtsStr + "}\n";
-
-        fs.writeFileSync(sigleDtsFilePath, dtsStr, "utf8");
-    })
-    // for (let i = 0; i < modules.length; i++) {
-    //     fileName = modules[i].id.includes("/") ? modules[i].id.split("/").pop() : modules[i].id.split("\\").pop();
-    //     if (fileName === "index.ts") {
-    //         continue;
-    //     }
-    //     dtsFilePath = path.join(process.cwd(), typesDir, fileName.replace(".ts", ".d.ts"));
-    //     dtsStr += fs.readFileSync(dtsFilePath, "utf8");
+            fs.writeFileSync(sigleDtsFilePath, dtsStr, "utf8");
+        })
+        // for (let i = 0; i < modules.length; i++) {
+        //     fileName = modules[i].id.includes("/") ? modules[i].id.split("/").pop() : modules[i].id.split("\\").pop();
+        //     if (fileName === "index.ts") {
+        //         continue;
+        //     }
+        //     dtsFilePath = path.join(process.cwd(), typesDir, fileName.replace(".ts", ".d.ts"));
+        //     dtsStr += fs.readFileSync(dtsFilePath, "utf8");
 
     // }
 }
