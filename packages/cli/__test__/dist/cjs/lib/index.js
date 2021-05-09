eee6e5691d938e58b3c62fdcafa4e0ad18e1e3d0
AILHC <505126057@qq.com>
2021-05-09 12:35:30 +0800

    fix: 修复命令行参数不起效的问题，修改为命令行参数配置优先 feature: 增加minjs的sourcemap输出配置
    affects: @ailhc/egf-cli

--- packages/cli/__test__/dist/cjs/lib/index.js
+++ packages/cli/__test__/dist/cjs/lib/index.js
@@ -2,26 +2,65 @@
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
-class classA {
-    sayA(a) { }
-}
+var classA = (function () {
+    function classA() {
+    }
+    classA.prototype.sayA = function (a) { };
+    return classA;
+}());
 
-class classB {
-    sayb(b) {
+var classB = (function () {
+    function classB() {
+    }
+    classB.prototype.sayb = function (b) {
         console.log("FDS");
-    }
-}
+    };
+    return classB;
+}());
 
-class classC {
-    sayd(cc) {
+var classC = (function () {
+    function classC() {
+    }
+    classC.prototype.sayd = function (cc) {
         console.log(cc);
-    }
-}
+    };
+    return classC;
+}());
 
-class bc {
-    constructor() {
+var bc = (function () {
+    function bc() {
         console.log("bc");
     }
+    return bc;
+}());
+
+/*! *****************************************************************************
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this file except in compliance with the License. You may obtain a copy of the
+License at http://www.apache.org/licenses/LICENSE-2.0
+
+THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+MERCHANTABLITY OR NON-INFRINGEMENT.
+
+See the Apache Version 2.0 License for specific language governing permissions
+and limitations under the License.
+***************************************************************************** */
+/* global Reflect, Promise */
+
+var extendStatics = function(d, b) {
+    extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return extendStatics(d, b);
+};
+
+function __extends(d, b) {
+    extendStatics(d, b);
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 }
 
 /*! *****************************************************************************
@@ -231,13 +270,16 @@ var App = (function () {
     return App;
 }());
 
-class RefOtherPkg extends App {
-    constructor() {
-        super();
-        const a = new App();
+var RefOtherPkg = (function (_super) {
+    __extends(RefOtherPkg, _super);
+    function RefOtherPkg() {
+        var _this = _super.call(this) || this;
+        var a = new App();
         console.log(a);
+        return _this;
     }
-}
+    return RefOtherPkg;
+}(App));
 
 exports.RefOtherPkg = RefOtherPkg;
 exports.bc = bc;
@@ -246,3 +288,4 @@ exports.classB = classB;
 exports.classC = classC;
 
     
+//# sourceMappingURL=index.js.map
--- packages/cli/__test__/testOption.js
+++ packages/cli/__test__/testOption.js
@@ -2,6 +2,8 @@
  * @type {IEgfCompileOption}
  */
 const option = {
+    /**测试命令行参数优先 */
+    format: "iife",
     footer: "testFoot"
 }
 module.exports = option;
