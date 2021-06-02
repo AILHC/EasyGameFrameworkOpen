96aecc14085e116dd33ac953d46cf70fbfde88fa
AILHC <505126057@qq.com>
2021-06-02 19:54:28 +0800

    chore: 增加编译配置文件，去掉旧的配置
    affects: @ailhc/broadcast, @ailhc/display-ctrl, @ailhc/dpctrl-c3d, @ailhc/dpctrl-fguicc,
    @ailhc/enet-pbws, @ailhc/enet, @ailhc/layer, @ailhc/obj-pool, c3daxios, c3demitter3, c3dxstate2

---
+++ packages/enet-pbws/egf.compile.js
@@ -0,0 +1,11 @@
+/**
+ * @type {import("@ailhc/egf-cli").IEgfCompileOption}
+ */
+const config = {
+
+    "dtsGenExclude": [
+        "__tests__/**/*",
+        "libs/**/*"
+    ]
+}
+module.exports = config;
