2f49cf8821b1cd0f1aa65626392c33dc5db72ce4
AILHC <505126057@qq.com>
2021-07-19 20:21:06 +0800

    docs: 文档更新、复制文档脚本更新

--- scripts/copyDocs.js
+++ scripts/copyDocs.js
@@ -1,3 +1,4 @@
+const packageRoots = ["./packages", "./tool-packages"];
 const cpr = require("cpr").cpr;
 //复制根目录的
 cpr("./README.md", "docs/README.md", {
@@ -10,20 +11,26 @@ cpr("./README.md", "docs/README.md", {
     console.error(err);
 })
 const path = require("path");
-cpr("./packages", "docs/packages", {
-    deleteFirst: true, //Delete "to" before
-    overwrite: true, //If the file exists, overwrite it
-    confirm: true, //After the copy, stat all the copied files to make sure they are there
-    // filter: (new RegExp(".json|.ts|.js|node_modules"))
-    filter: function (file) {
-        if (file.includes("node_modules")) return false;
-        if (file.includes(".md")) {
-            return true;
+let fromPath;
+let toPath;
+for (let i = 0; i < packageRoots.length; i++) {
+    fromPath = packageRoots[i];
+    toPath = path.join("./docs",fromPath);
+    cpr(fromPath, toPath, {
+        deleteFirst: true, //Delete "to" before
+        overwrite: true, //If the file exists, overwrite it
+        confirm: true, //After the copy, stat all the copied files to make sure they are there
+        // filter: (new RegExp(".json|.ts|.js|node_modules"))
+        filter: function (file) {
+            if (file.includes("node_modules")) return false;
+            if (file.includes(".md")) {
+                return true;
+            }
+            return false;
         }
-        return false;
-    }
-}, function (err, files) {
-    //err - The error if any (err.list might be available with an array of errors for more detailed information)
-    //files - List of files that we copied
-    err && console.error(err);
-})
+    }, function (err, files) {
+        //err - The error if any (err.list might be available with an array of errors for more detailed information)
+        //files - List of files that we copied
+        err && console.error(err);
+    })
+}
