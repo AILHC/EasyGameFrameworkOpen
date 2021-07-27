461c57d22681f616f34d6f628a50081c478f8b6e
AILHC <505126057@qq.com>
2021-07-27 21:24:40 +0800

    refactor: 初步开始实现
    affects: @ailhc/display-ctrl-next

---
+++ packages/display-ctrl-next/src/define-display-ctrl.ts
@@ -0,0 +1,12 @@
+export const globalCtrlTemplateMap: displayCtrl.CtrlTemplateMap = {};
+/**
+ * 定义显示控制器模板
+ * @param ctrlTemplate 显示控制器模板
+ * @param templateMap 默认为全局模板字典，可自定义
+ */
+export function defineDisplayCtrlTemplate<GetRessParams = any, CreateParams = any>(
+    ctrlTemplate: displayCtrl.ICtrlTemplate<GetRessParams, CreateParams>,
+    templateMap: displayCtrl.CtrlTemplateMap = globalCtrlTemplateMap
+) {
+    globalCtrlTemplateMap[ctrlTemplate.key] = ctrlTemplate;
+}
