7a09b5517fc3cdc9053dd4c5a6b7599784b54eee
AILHC <505126057@qq.com>
2021-08-05 21:26:27 +0800

    test: 增加单元测试大纲
    affects: @ailhc/display-ctrl-next

---
+++ packages/display-ctrl-next/__tests__/dpc-mgr-next-regist.test.ts
@@ -0,0 +1,13 @@
+describe(`注册模板相关单元测试`, function () {
+    test(`测试注册模板`, function () {});
+
+    test(`测试模板是否注册`, function () {});
+
+    test(`测试重复注册模板`, function () {});
+
+    test(`测试批量注册模板`, function () {});
+
+    test(`测试批量注册模板`, function () {});
+
+    test(`测试注册Dpc类模板`, function () {});
+});
