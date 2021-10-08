0ba2b22642d6a6c86dadf4bff976de33f45e0b99
AILHC <505126057@qq.com>
2021-10-08 21:16:14 +0800

    WIP: 增加模板相关单元测试，修复测试遇到的问题，完善资源加载、持有、释放、销毁逻辑
    affects: @ailhc/display-ctrl-next

---
+++ packages/display-ctrl-next/__tests__/dpc-mgr-next-resource.test_no.ts
@@ -0,0 +1,33 @@
+// describe(`资源相关测试`, function () {
+//     test(`测试获取模板依赖的资源 getDpcRess 返回字符串数组或资源对象数组`, function () {});
+
+//     test(`测试通用模板依赖的资源加载 loadDpcRess`, function () {});
+
+//     test(`测试模板自定义资源加载逻辑 loadDpcRess`, function () {});
+// });
+
+// describe(`Dpc实例操作相关测试`, function () {
+//     test(`测试实例化Dpc`, function () {});
+//     test(`测试初始化指定Dpc实例`, function () {});
+//     test(`测试显示指定Dpc实例`, function () {});
+//     test(`测试更新指定Dpc实例`, function () {});
+//     test(`测试隐藏指定Dpc实例`, function () {});
+//     test(`测试销毁指定Dpc实例`, function () {});
+//     test(`测试销毁指定Dpc实例`, function () {});
+// });
+
+// describe(`单例Dpc相关测试`, function () {
+//     test(`测试获取单例Dpc实例`, function () {});
+
+//     test(`测试显示Dpc`, function () {});
+
+//     test(`测试显示加载完成后的Dpc`, function () {});
+
+//     test(`测试更新指定单例Dpc`, function () {});
+
+//     test(`测试更新指定未加载完成的单例Dpc`, function () {});
+
+//     test(`测试隐藏指定单例Dpc`, function () {});
+
+//     test(`测试销毁指定单例Dpc`, function () {});
+// });
