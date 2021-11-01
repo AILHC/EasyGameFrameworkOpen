fe577535fbaf0e184ec6435562ee17c022c0e548
AILHC <505126057@qq.com>
2021-11-01 09:15:33 +0800

    WIP: 完善viewState相关机制，修复bug，跑通单元测试
    affects: @ailhc/akview
    
    1. 去掉viewState的按类型注册生成机制
    2. 增加从templateHandler和template中创建viewState的机制
    3. 修复单元测试遇到的bug

--- packages/akview/__tests__/create-show-destroy.test.ts
+++ packages/akview/__tests__/create-show-destroy.test.ts
@@ -3,9 +3,11 @@ describe(`显示(show)-创建(create)-销毁(destroy)相关接口单元测试`, 
     test(`测试无资源加载的单例View相关接口测试(show、hide、update、destroy)`, function (testDone) {
         const uiMgr = new ViewMgr();
         uiMgr.init();
+        const uiMgrRetainResSpy = spyOn(uiMgr, "retainTemplateRes");
+        const uiMgrReleaseResSpy = spyOn(uiMgr, "releaseTemplateRes");
         const testTemplate: akView.ITemplate = {
             key: "testTemplate",
-            create() {
+            createViewIns() {
                 const viewIns: akView.IView = {
                     onViewInit(initCfg) {
                         expect(initCfg.onInitData).toBeUndefined();
@@ -50,6 +52,7 @@ describe(`显示(show)-创建(create)-销毁(destroy)相关接口单元测试`, 
                 expect(viewIns.key).toEqual(testTemplate.key);
             }
         } as akView.IShowConfig);
+        expect(uiMgrRetainResSpy).toBeCalledTimes(1);
         expect(uiMgr.isInited(testTemplate.key)).toBeTruthy();
         expect(uiMgr.isShowed(testTemplate.key)).toBeTruthy();
 
@@ -58,20 +61,23 @@ describe(`显示(show)-创建(create)-销毁(destroy)相关接口单元测试`, 
 
         uiMgr.hide(testTemplate.key);
         expect(spyOnViewHide).toBeCalledTimes(1);
-
+        expect(uiMgrReleaseResSpy).toBeCalledTimes(0);
         uiMgr.show(testTemplate.key, { test: "a" });
+        expect(uiMgrRetainResSpy).toBeCalledTimes(1);
         expect(uiMgr.isShowed(testTemplate.key)).toBeTruthy();
         expect(spyOnViewInit).toBeCalledTimes(1);
         expect(spyOnViewShow).toBeCalledTimes(2);
-        expect(uiMgr["_viewStateMap"][testTemplate.key].retainTemplateRes).toBeTruthy();
-        uiMgr.hide(testTemplate.key, undefined, true);
-        expect(uiMgr["_viewStateMap"][testTemplate.key].retainTemplateRes).toBeFalsy();
-
+        expect(uiMgr["_viewStateMap"][testTemplate.key].isRetainTemplateRes).toBeTruthy();
+        uiMgr.hide(testTemplate.key, { releaseRes: true });
+        expect(uiMgr["_viewStateMap"][testTemplate.key].isRetainTemplateRes).toBeFalsy();
+        expect(uiMgrReleaseResSpy).toBeCalledTimes(1);
         uiMgr.destroy(testTemplate.key);
         expect(spyOnViewDestroy).toBeCalledTimes(1);
+        expect(uiMgrReleaseResSpy).toBeCalledTimes(1);
+
         expect(uiMgr.isShowed(testTemplate.key)).toBeFalsy();
         expect(uiMgr.isInited(testTemplate.key)).toBeFalsy();
-        expect(uiMgr["_viewStateMap"][testTemplate.key].retainTemplateRes).toBeFalsy();
+        expect(uiMgr["_viewStateMap"][testTemplate.key]).toBeUndefined();
         testDone();
     });
     test(`测试无资源加载的多例View相关接口测试(show、hide、update、destroy)`, function (testDone) {
@@ -79,7 +85,7 @@ describe(`显示(show)-创建(create)-销毁(destroy)相关接口单元测试`, 
         uiMgr.init();
         const testTemplate: akView.ITemplate = {
             key: "testTemplate",
-            create() {
+            createViewIns() {
                 const viewIns: akView.IView = {
                     onViewInit(initCfg) {
                         expect(initCfg.onInitData).toBeDefined();
@@ -131,25 +137,25 @@ describe(`显示(show)-创建(create)-销毁(destroy)相关接口单元测试`, 
 
         expect(uiMgr.isShowedById(testViewId)).toBeTruthy();
 
-        uiMgr.update(testViewId, "a");
+        uiMgr.updateById(testViewId, "a");
         expect(spyOnViewUpdate).toBeCalledTimes(1);
         expect(spyOnViewShow).toBeCalledTimes(1);
-        uiMgr.hide(testViewId);
+        uiMgr.hideById(testViewId);
         expect(spyOnViewHide).toBeCalledTimes(1);
 
-        uiMgr.show(testViewId, { test: "a" });
-        expect(uiMgr.isShowed(testViewId)).toBeTruthy();
+        uiMgr.showById(testViewId, { onShowData: { test: "a" } });
+        expect(uiMgr.isShowedById(testViewId)).toBeTruthy();
         expect(spyOnViewInit).toBeCalledTimes(1);
         expect(spyOnViewShow).toBeCalledTimes(2);
-        expect(uiMgr["_viewStateMap"][testTemplate.key].retainTemplateRes).toBeTruthy();
-        uiMgr.hide(testTemplate.key, undefined, true);
-        expect(uiMgr["_viewStateMap"][testTemplate.key].retainTemplateRes).toBeFalsy();
+        expect(uiMgr["_viewStateMap"][testViewId].isRetainTemplateRes).toBeTruthy();
+        uiMgr.hideById(testViewId, { releaseRes: true });
+        expect(uiMgr["_viewStateMap"][testViewId].isRetainTemplateRes).toBeFalsy();
 
-        uiMgr.destroy(testTemplate.key);
+        uiMgr.destroyById(testViewId);
         expect(spyOnViewDestroy).toBeCalledTimes(1);
-        expect(uiMgr.isShowed(testTemplate.key)).toBeFalsy();
-        expect(uiMgr.isInited(testTemplate.key)).toBeFalsy();
-        expect(uiMgr["_viewStateMap"][testTemplate.key].retainTemplateRes).toBeFalsy();
+        expect(uiMgr.isShowedById(testViewId)).toBeFalsy();
+        expect(uiMgr.isInitedById(testViewId)).toBeFalsy();
+        expect(uiMgr.getViewState(testViewId).isRetainTemplateRes).toBeFalsy();
         testDone();
     });
 });
